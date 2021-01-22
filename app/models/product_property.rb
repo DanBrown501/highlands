c262bb071f29a0e92c65c51bba4625eab95e5952
DanBrown501 <62338644+DanBrown501@users.noreply.github.com>
2021-01-21 23:31:32 -0600

    Delete app directory

--- app/models/product_property.rb
+++
@@ -1,5 +0,0 @@
-class ProductProperty < ApplicationRecord
-  validates :value, presence: true, length: { maximum: 255 }
-  belongs_to :property, required: false
-  belongs_to :product, required: false
-end
--- app/models/property.rb
+++
@@ -1,6 +0,0 @@
-class Property < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 255 }, uniqueness: true
-  has_many :product_properties
-  has_many :products, through: :product_properties
-  accepts_nested_attributes_for :product_properties
-end
